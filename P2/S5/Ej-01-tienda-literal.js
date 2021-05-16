d9aadf8bd93740c208570a4797e09a851352d4dd
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2021-05-16 18:11:03 +0200

    tienda literal

--- P2/S5/Ej-01-tienda-literal.js
+++ P2/S5/Ej-01-tienda-literal.js
@@ -1,7 +1,7 @@
 //-- Crear la variable tienda, que es una estructura formada
 //-- por dos productos
 const tienda = [
-  {
+  { //-- Lista de productos a la venta.
     nombre: "Alhambra II",
     descripcion: "Placa con FPGA ice40HX8K",
     stock: 3
@@ -12,10 +12,11 @@ const tienda = [
   }
 ];
 
-//-- Mostrar informacion sobre la tienda
+//-- Mostrar informacion sobre la tienda utilizando el variable.length
 console.log("Productos en la tienda: " + tienda.length);
 
 //-- Recorrer el array de productos
 tienda.forEach((element, index)=>{
-  console.log("Producto: " + (index + 1) + ": " + element.nombre);
+  console.log("Producto: " + (index + 1) + ": " + element.nombre +
+               " stock: " + element.stock);
 });
