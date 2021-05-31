b02ad8578c7618c46ba55334bd5ff4d4b3ac4708
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2021-06-01 00:05:16 +0200

    index 2

--- P4/chat/index.js
+++ P4/chat/index.js
@@ -1,44 +1,53 @@
+const ip = require('ip');
 const electron = require('electron');
+const QRCode = require('qrcode');
 
 console.log("Hola desde el proceso de la web...");
 
 //-- Obtener elementos de la interfaz
-const btn_test = document.getElementById("btn_test");
-const display = document.getElementById("display");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
-const print = document.getElementById("print");
+const info4 = document.getElementById("info4");
+const info_users = document.getElementById("info_users");
+const mensajes = document.getElementById("mensajes");
+const mensaje = document.getElementById("mensaje");
+const button_test = document.getElementById("button_test");
+const qr = document.getElementById("qr");
 
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
 //-- el proceso princpal
-info1.textContent = process.arch;
-info2.textContent = process.platform;
-info3.textContent = process.cwd();
+info1.textContent = process.versions.node;
+info2.textContent = process.versions.chrome;
+info3.textContent = process.versions.electron;
+info_users.textContent = 0;
 
+// Creamos el código QR
+const src = 'http://' + ip.address() + ':' + '8080';
+QRCode.toDataURL(src, function (err, url) {
+    qr.src = url;
+});
 
-btn_test.onclick = () => {
-    display.innerHTML += "TEST! ";
+electron.ipcRenderer.on('print-ip', (event, message) => {
+    console.log("Recibido: " + message);
+    info4.innerHTML = message;
+});
+
+electron.ipcRenderer.on('print-users', (event, message) => {
+    console.log("Recibido: " + message);
+    info_users.innerHTML = message;
+});
+
+electron.ipcRenderer.on('print-msg', (event, message) => {
+    console.log("Recibido: " + message);
+    mensajes.innerHTML += message + '<br>';
+    mensaje.scrollTop = mensaje.scrollHeight;
+});
+
+button_test.onclick = () => {
     console.log("Botón apretado!");
 
     //-- Enviar mensaje al proceso principal
-    electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Boton apretado");
+    electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: ¡Que la fuerza te acompañe!");
 }
-socket.on("message", (msg)=>{
-
-    display.innerHTML = '<p style="color:blue">' + msg + '</p>' + display.innerHTML;
-  
-  });
-msg_entry.onchange = () => {
-    if (msg_entry.value)
-    //-- Borrar el mensaje actual
-    msg_entry.value = "";
-}
-
-
-electron.ipcRenderer.on('print-msg', (event, message) => {
-  console.log("Recibido: " + message);
-  mensajes.innerHTML += message + '<br>';
-  mensaje.scrollTop = mensaje.scrollHeight;
-});
