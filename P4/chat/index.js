1e94f92e4c312846b9f6ed88501262b97c99da0a
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2021-06-01 01:05:22 +0200

    indexjs

--- P4/chat/index.js
+++ P4/chat/index.js
@@ -2,13 +2,11 @@ const ip = require('ip');
 const electron = require('electron');
 const QRCode = require('qrcode');
 
-console.log("Hola desde el proceso de la web...");
-
 //-- Obtener elementos de la interfaz
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
-const info4 = document.getElementById("info4");
+const direccion_ip = document.getElementById("direccionip");
 const info_users = document.getElementById("info_users");
 const mensajes = document.getElementById("mensajes");
 const mensaje = document.getElementById("mensaje");
@@ -22,16 +20,14 @@ info1.textContent = process.versions.node;
 info2.textContent = process.versions.chrome;
 info3.textContent = process.versions.electron;
 info_users.textContent = 0;
+info_1.textContent = process.arch;
+info_2.textContent = process.platform;
+info_3.textContent = process.cwd();
 
-// Creamos el código QR
-const src = 'http://' + ip.address() + ':' + '8080';
-QRCode.toDataURL(src, function (err, url) {
-    qr.src = url;
-});
 
 electron.ipcRenderer.on('print-ip', (event, message) => {
     console.log("Recibido: " + message);
-    info4.innerHTML = message;
+    direccion_ip.textContent = message;
 });
 
 electron.ipcRenderer.on('print-users', (event, message) => {
@@ -41,7 +37,7 @@ electron.ipcRenderer.on('print-users', (event, message) => {
 
 electron.ipcRenderer.on('print-msg', (event, message) => {
     console.log("Recibido: " + message);
-    mensajes.innerHTML += message + '<br>';
+    mensajes.innerHTML = message + '<br>' + mensajes.innerHTML;
     mensaje.scrollTop = mensaje.scrollHeight;
 });
 
@@ -49,5 +45,5 @@ button_test.onclick = () => {
     console.log("Botón apretado!");
 
     //-- Enviar mensaje al proceso principal
-    electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: ¡Que la fuerza te acompañe!");
+    electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Alea Iacta Est 'La suerte está echada'");
 }
