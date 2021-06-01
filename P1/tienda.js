191b1f16126034c0ea8bd9d8c485a0ac8c12d5f8
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2021-06-01 20:41:18 +0200

    tienda

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,3 +1,4 @@
+//-- Creación de las constantes
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
@@ -7,17 +8,14 @@ const PUERTO = 9000;
 http.createServer((req, res) => {
   console.log("----------> Peticion recibida")
   let q = url.parse(req.url, true);
-  console.log("Recurso:" + q.pathname)
-  var recurso = q.pathname.split("/")[2];
+  console.log("Recurso:" + q.pathname);
   var tipo = q.pathname.split(".")[1]
-  const cookie = req.headers.cookie;
-  const params = q.query;
   
   let filename = "";
 
   //-- Obtener fichero a devolver
   switch (q.pathname) {
-
+    
     //-- Pagina principal
     case "/":
       filename = "index.html";
@@ -26,9 +24,9 @@ http.createServer((req, res) => {
     default:
     filename = q.pathname.substr(1);
   }
-    var extension = tipo;
     let mime = "";
-    switch (extension) {
+   
+    switch (tipo) {
       case "js":
         mime = "application/js"
       case "json":
